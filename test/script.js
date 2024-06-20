f69d08c6216595eaf15b04bf33e3b5f2d23048d4
bulmang <hmk113737@gmail.com>
2024-06-20 14:10:38 +0900

    initial commit

---
+++ test/script.js
@@ -0,0 +1,10 @@
+// script.js 파일
+
+document.addEventListener("DOMContentLoaded", function() {
+  const button = document.getElementById("testButton");
+
+  button.addEventListener("click", function() {
+    alert("Button clicked!");
+    // 여기에 버튼 클릭 시 수행할 동작을 추가할 수 있습니다.
+  });
+});
